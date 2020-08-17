0f93c693ccc369e5716c174d0d38e81831c111b9
Mahaboob Basha <mahaboobbasha2109@gmail.com>
2020-08-17 16:36:38 +0530

    Input-arguments

--- 05-input-arguments.sh
+++ 05-input-arguments.sh
@@ -1,13 +1,1 @@
 #!/bin/bash 
-
-# Special variables with regards to inputs, $0-$n, $*, $@, $# 
-
-echo Script Name = $0 
-echo First Argument = $1
-echo Second Argument = $2
-## Beyond 9 , From 10 argument ${}
-
-echo All arguments = $* 
-echo All arguments = $@
-
-echo Number of Arguments = $#
