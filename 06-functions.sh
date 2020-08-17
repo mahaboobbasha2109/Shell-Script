e178c517b3c64a6f88747b0f7391d3a7b1c1dcfb
Mahaboob Basha <mahaboobbasha2109@gmail.com>
2020-08-17 18:05:00 +0530

    functions

---
+++ 06-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#variable holds data, Function holds commands, when you call function it execute all the commands declared in function.
+
+# Declare function 
+
+sample(){
+    echo Hello World from Function
+}
+
+# call the function 
+
+sample
