0a3e21c6eb3a9dd69c0361bc4e9c58dcccd9e697
Mahaboob Basha <mahaboobbasha2109@gmail.com>
2020-08-17 12:52:57 +0530

    print

--- 02-print.sh
+++ 02-print.sh
@@ -6,4 +6,35 @@ echo Hello World
 echo
 echo Hello Universe
 
+# Print Multiple lines
+
 echo -e "Hello World\nHello Universe"
+
+# When you are using \n to print new lines then quote the input, Also enable -e option.
+# Color Codes
+# Color         Code
+################################
+# Red         | 31
+# Green       | 32
+# Yellow      | 33
+# Blue        | 34
+# Magenta     | 35
+# Cyna        | 36
+
+# echo -e "\e[COLOR-CODEmMESSAGE"
+
+echo -e "\e[31mHello"
+echo -e "\e[32mHello"
+echo -e "\e[33mHello"
+echo -e "\e[34mHello"
+echo -e "\e[35mHello"
+echo -e "\e[36mHello"
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+# Also very important thing we need to keep in mind is , When we enable a color we need to disable that 
+
+echo This line should not print any color 
+
+echo -e "\e[31mHello World\e[0m"
+echo This line should not print any color 
