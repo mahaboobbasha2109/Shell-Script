e3b134d0368d41e6917412c5c024ee3993fe3d3f
Mahaboob Basha <mahaboobbasha2109@gmail.com>
2020-08-18 09:13:28 +0530

    If-Condition

---
+++ 09-if-condition.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# simple if 
+# if [ expression ]; then 
+#   commands
+# fi
+#### If expression is true then if command executes the commands inside that.
+
+# If else 
+# if [ expression ]; then 
+#   commands
+# else 
+#   commands
+# fi 
+
+# Else If 
+# if [ expression1 ]; then 
+#   commands1
+# elif [ expression2 ]; then 
+#   commands2
+# else
+#   commands
+# fi
+
+## Expressions 
+# 1. String Comparision Operators: ==, !=
+# Ex : [ abc  == abc ]
+# 2. Number Comparision Operators : -eq, -ne, -lt, -le, -gt, -ge 
+# [ 0 -gt 1 ]
